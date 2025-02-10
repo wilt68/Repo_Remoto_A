ea8b372d748b83ea6b7d6a0c6e7429b5e268a811
unknown <gfraile@ieszayas.org>
2025-02-10 20:15:44 +0100

    cambios desde local v4.4

--- desarrolloV4.java
+++ desarrolloV4.java
@@ -5,4 +5,6 @@ iniciamos trabajo en nueva version - V4
   Resuelto rpoblema gestión matrices, nueva versión V4.2
 
   Seguimos trabajando en remoto, nueva version V4.3
+
+Modificacion desde local, nueva version v4.4
   
