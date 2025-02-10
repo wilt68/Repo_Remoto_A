f86329a7ab54a224dcf470686127818de40b9cd1
wilt68 <75388101+wilt68@users.noreply.github.com>
2025-02-10 15:12:46 +0100

    version 4.5

--- desarrolloV4.java
+++ desarrolloV4.java
@@ -8,4 +8,6 @@ SEguimos trabajando en la rama develop desde LOCAL v4.3
 
   Seguimois trabajando en la rama develop desde origin, nueva ver5sion v4.4
 
+  Seguimos trabajando en la rama develop desde origin, nueva version v4.5
+
   
