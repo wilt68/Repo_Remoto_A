3e9649fda51cfd183b71502a7a09180c6cd9fa18
wilt68 <75388101+wilt68@users.noreply.github.com>
2025-02-10 20:12:16 +0100

    Version 4.3

--- desarrolloV4.java
+++ desarrolloV4.java
@@ -3,4 +3,6 @@ iniciamos trabajo en nueva version - V4
   Seguimos trabajando en la n ueva version V4.1
 
   Resuelto rpoblema gestión matrices, nueva versión V4.2
+
+  Seguimos trabajando en remoto, nueva version V4.3
   
