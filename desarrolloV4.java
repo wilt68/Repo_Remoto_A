289cb096e74c3d5bb99d2b122bfa13cee3664654
Guillermo <guillermo68@gmail.com>
2025-02-10 11:29:07 +0100

    version 4.3 desde local

--- desarrolloV4.java
+++ desarrolloV4.java
@@ -3,4 +3,7 @@ iniciamos trabajo en nueva version - V4
   Seguimos trabajando en la n ueva version V4.1
 
   Resuelto rpoblema gestión matrices, nueva versión V4.2
+
+SEguimos trabajando en la rama develop desde LOCAL v4.3
+
   
